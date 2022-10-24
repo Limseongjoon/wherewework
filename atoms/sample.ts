9a6b907a75b7dfd398baf939297676747e622946
Sangwon <born9507@snu.ac.kr>
2022-10-24 16:35:02 +0900

    [feat] Set up boilerplate

---
+++ atoms/sample.ts
@@ -0,0 +1,8 @@
+import { atom } from 'recoil';
+
+export const sampleState = atom({
+  key: 'sampleState',
+  default: [],
+});
+
+// https://recoiljs.org/ko/docs/introduction/getting-started
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,80 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { ChakraProvider } from '@chakra-ui/react';
+import { RecoilRoot } from 'recoil';
+import { extendTheme } from '@chakra-ui/react';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import * as gtag from '../lib/gtag';
+import ChannelService from '../lib/ChannelService';
+import Script from 'next/script';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+// https://chakra-ui.com/getting-started/nextjs-guide
+const colors = {
+  // brand: {
+  //   900: '#1a365d',
+  //   800: '#153e75',
+  //   700: '#2a69ac',
+  // },
+};
+
+const theme = extendTheme({ colors });
+
+function App({ Component, pageProps }: AppProps) {
+  // GA
+  // const router = useRouter();
+  // useEffect(() => {
+  //   const handleRouteChange = (url: any) => {
+  //     gtag.pageview(url);
+  //   };
+  //   router.events.on('routeChangeComplete', handleRouteChange);
+  //   router.events.on('hashChangeComplete', handleRouteChange);
+  //   return () => {
+  //     router.events.off('routeChangeComplete', handleRouteChange);
+  //     router.events.off('hashChangeComplete', handleRouteChange);
+  //   };
+  // }, [router.events]);
+
+  // 채널톡
+  // useEffect(() => {
+  //   const channelTalk = new ChannelService();
+
+  //   channelTalk.boot({
+  //     pluginKey: process.env.NEXT_PUBLIC_CHANNEL_IO_KEY,
+  //   });
+  //   return () => {
+  //     channelTalk.shutdown();
+  //   };
+  // }, []);
+
+  return (
+    <>
+      {/* GA */}
+      {/* <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      /> */}
+      <RecoilRoot>
+        <ChakraProvider theme={theme}>
+          <Component {...pageProps} />
+        </ChakraProvider>
+      </RecoilRoot>
+    </>
+  );
 }
 
-export default MyApp
+export default App;
