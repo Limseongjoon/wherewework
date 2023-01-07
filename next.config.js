0d76e124d66a92c159aa884557d29fd48a77a6cb
Sangwon <born9507@snu.ac.kr>
2023-01-07 16:51:25 +0900

    [fix] Fix typo

--- next.config.js
+++ next.config.js
@@ -42,4 +42,4 @@ const sentryWebpackPluginOptions = {
 // Make sure adding Sentry options is the last code to run before exporting, to
 // ensure that your source maps include changes from all other Webpack plugins
 // module.exports = withSentryConfig(nextConfig, sentryWebpackPluginOptions);
-module.export = nextConfig;
+module.exports = nextConfig;
