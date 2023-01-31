4ff24417ded235d73c263c25a26c892d7fa394af
Sangwon <born9507@snu.ac.kr>
2023-01-31 22:06:53 +0900

    [feat] Add locofy codes

---
+++ pages/web/contact.tsx
@@ -0,0 +1,76 @@
+import type { NextPage } from 'next';
+
+const FrameComponent1: NextPage = () => {
+  return (
+    <div className="relative bg-darkslategray-200 w-full h-[2454px] overflow-hidden text-left text-9xl text-white font-pretendard">
+      <img
+        className="absolute top-[4940.41px] left-[279.58px] w-[1333.47px] h-[251.06px] object-cover hidden"
+        alt=""
+        src="../image-191@2x.png"
+      />
+      <b className="absolute top-[107.57px] left-[1364.9px] tracking-[0.05em]">
+        CONTACT
+      </b>
+      <div className="absolute top-[91px] left-[1580px] w-[209px] h-[57px] text-13xl">
+        <div className="absolute top-[0px] left-[0px] rounded-xl bg-mediumslateblue w-[209px] h-[57px]" />
+        <b className="absolute top-[14.48px] left-[59px] tracking-[0.05em]">
+          신청하기
+        </b>
+      </div>
+      <b className="absolute top-[93px] left-[146.29px] text-25xl font-gmarket-sans-ttf">
+        URLank
+      </b>
+      <div className="absolute top-[2145px] left-[0px] w-[1920px] h-[308.82px] text-22xl">
+        <div className="absolute top-[0px] left-[0px] bg-mediumslateblue w-[1920px] h-[308.82px]" />
+        <b className="absolute top-[82.21px] left-[891px]">URLank</b>
+        <div className="absolute top-[194.26px] left-[764px] text-12xl font-medium">
+          © BASER. ALL RIGHTS RESERVED.
+        </div>
+      </div>
+      <div className="absolute top-[385px] left-[251px] w-[1417px] h-[1527px] text-16xl">
+        <div className="absolute top-[0px] left-[0px] rounded-2xxl bg-white w-[1417px] h-[1527px]" />
+        <div className="absolute top-[1361px] left-[411px] w-[567px] h-[100px]">
+          <div className="absolute top-[0px] left-[0px] rounded-5xxl bg-mediumslateblue box-border w-[567px] h-[100px] border-[2px] border-solid border-black" />
+          <b className="absolute top-[32.78px] left-[198.24px] tracking-[0.06em]">
+            메일 전송하기
+          </b>
+        </div>
+        <b className="absolute top-[71px] left-[110px] text-28xl tracking-[0.06em] text-black">
+          CONTACT
+        </b>
+        <div className="absolute top-[306px] left-[110px] w-[1170px] h-[947px] text-21xl text-dimgray">
+          <div className="absolute top-[223px] left-[0px] w-[1170px] h-[724px]">
+            <div className="absolute top-[68px] left-[0px] rounded-xxl bg-ghostwhite w-[1170px] h-[656px]" />
+            <b className="absolute top-[0px] left-[0px] tracking-[0.06em]">
+              문의 및 전달내용
+            </b>
+          </div>
+          <div className="absolute top-[0px] left-[3px] w-[555.85px] h-[146.76px]">
+            <b className="absolute top-[0px] left-[0px] tracking-[0.06em]">
+              이름
+            </b>
+            <div className="absolute top-[71px] left-[0px] w-[555.85px] h-[75.76px] text-3xl text-darkgray-200">
+              <div className="absolute top-[0px] left-[0px] rounded-xxl bg-ghostwhite w-[555.85px] h-[75.76px]" />
+              <div className="absolute top-[26.7px] left-[37.66px] font-medium">
+                Full Name
+              </div>
+            </div>
+          </div>
+          <div className="absolute top-[0px] left-[614px] w-[555.85px] h-[146.76px]">
+            <b className="absolute top-[0px] left-[0px] tracking-[0.06em]">
+              메일 주소
+            </b>
+            <div className="absolute top-[71px] left-[0px] w-[555.85px] h-[75.76px] text-3xl text-darkgray-200">
+              <div className="absolute top-[0px] left-[0px] rounded-xxl bg-ghostwhite w-[555.85px] h-[75.76px]" />
+              <div className="absolute top-[26.7px] left-[37.66px] font-medium">
+                Enter your email address
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FrameComponent1;
