5167b3eb3e75ad616b3e960ab9c8d26bc324e1e7
Sangwon <born9507@snu.ac.kr>
2023-02-07 17:52:22 +0900

    [fix] Fix sanity error

--- lib/client.ts
+++ lib/client.ts
@@ -1,15 +1,15 @@
 // supabase, pocketbase, sanity;
-import sanityClient from '@sanity/client';
+// import sanityClient from '@sanity/client';
 import { createClient } from '@supabase/supabase-js';
 import { Session } from 'next-auth';
 
-export const sanity = sanityClient({
-  projectId: process.env.SANITY_PROJECT_ID, // you can find this in sanity.json
-  dataset: process.env.SANITY_DATASET,
-  token: process.env.SANITY_API_TOKEN,
-  useCdn: process.env.NODE_ENV === 'production', // `false` if you want to ensure fresh data
-  // apiVersion: '2021-10-21',
-});
+// export const sanity = sanityClient({
+//   projectId: process.env.SANITY_PROJECT_ID, // you can find this in sanity.json
+//   dataset: process.env.SANITY_DATASET,
+//   token: process.env.SANITY_API_TOKEN,
+//   useCdn: process.env.NODE_ENV === 'production', // `false` if you want to ensure fresh data
+//   // apiVersion: '2021-10-21',
+// });
 
 export const supabase = (session?: Session | null) => {
   return createClient(
