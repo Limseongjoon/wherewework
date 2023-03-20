2076ed9909653b0a86e3f8cf769cbc3258fb72c2
Limseongjoon <sungjun1703@snu.ac.kr>
2023-03-20 13:42:11 +0900

    [feat]pic

--- pages/team.tsx
+++ pages/team.tsx
@@ -6,7 +6,7 @@ const Page = () => {
     <div className="flex flex-col items-center w-screen max-h-fit bg-white text-29xl font-pretendard">
       <Header />
       <div className="h-64"></div>
-      <img className="w-10/12 lg:w-[980px]" alt="" src="/images/team.png" />
+      <img className="w-10/12 lg:w-[980px]" alt="" src="/images/wwwteam.png" />
       <div className="h-32 lg:h-36"></div>
       <div className="w-full h-[120px] lg:h-[240px] flex flex-col items-center justify-center gap-4 lg:gap-10 bg-black text-white text-center">
         <div className="w-full text-base lg:text-10xl font-bold tracking-wider">
